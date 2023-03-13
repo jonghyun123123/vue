47803caee9e6431849cfe289c3a585b5d89567a0
jonghyun123123 <124219998+jonghyun123123@users.noreply.github.com>
2023-03-13 17:03:44 +0900

    Add files via upload

---
+++ assets/product.js
@@ -0,0 +1,43 @@
+export default[{
+    id:0,
+    title:'그랜드 엠포리엄',
+    image:'https://jonghyun123123.github.io/imgs/a.png',
+    content:'그랜드 엠포리엄에 놀러오세요~!',
+    price:500000
+},
+{
+    id:1,
+    title:'타운 센터 패션',
+    image:'https://jonghyun123123.github.io/imgs/b.png',
+    content:'타운 센터 패션에 놀러오세요~!',
+    price:1000000
+},
+{
+    id:2,
+    title:'토이 스테이션',
+    image:'https://jonghyun123123.github.io/imgs/c.png',
+    content:'토이 스테이션에 놀러오세요~!',
+    price:2000000
+},
+{
+    id:3,
+    title:'미키 마우스',
+    image:'https://jonghyun123123.github.io/imgs/d.png',
+    content:'귀여운 미키 마우스 인형!!',
+    price:70000
+},
+{
+    id:4,
+    title:'미니 마우스',
+    image:'https://jonghyun123123.github.io/imgs/e.png',
+    content:'귀여운 미니 마우스 인형!!',
+    price:40000
+},
+{
+    id:5,
+    title:'도널드덕',
+    image:'https://jonghyun123123.github.io/imgs/f.png',
+    content:'귀여운 도널드덕 인형!!',
+    price:50000
+}
+]
